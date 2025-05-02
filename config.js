5f0647bbbcdf8f2089f9a36786cdc9702777c021
Siyamui <siyamkingff06@gmail.com>
2025-05-02 18:09:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SADIYA-MD=TAURVbpS#BoWyjNvr8WjCMDOcQsbplYGJ15orap0GxUhjwgMLprw", //Put Session-id Here
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "https://github.com/Siyamui", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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", //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
 SESSION_NAME: process.env.SESSION_NAME || "SADIYA-MD=TAURVbpS#BoWyjNvr8WjCMDOcQsbplYGJ15orap0GxUhjwgMLprw"
 };
